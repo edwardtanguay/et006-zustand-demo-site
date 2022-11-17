ea9c59d351801dee77fd541099d4c50c0a962cba
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-17 09:59:39 +0100

    simple state variable in (message)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import './App.scss'
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+	const [message, setMessage] = useState('test');
+
 	return (
 		<div className="App">
-		<h2>Zustand Demo</h2>
-		<p>Welcome to the demo.</p>
+			<h2>Zustand Demo</h2>
+
+			<main>
+				<section className="controlArea">
+					<div className="data">
+						<label>Message:</label>
+						<input
+							type="text"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+						Message: {message}
+					</div>
+				</section>
+			</main>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
